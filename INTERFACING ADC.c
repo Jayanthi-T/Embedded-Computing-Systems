2b50a8411212255f242cdce7dfbcc469dfe1f7fb
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2020-10-14 21:46:26 +0530

    INTERFACING ADC
    AIM:
    To write an embedded C program for interfacing ADC and DAC and verify the output
    in the ARM kit.
    HARDWARE AND SOFTWARE REQUIRED:
    PC
    EMBEST IDE
    ARM Evaluation kit
    ALGORITHM:
    ADC:
    
    DAC:
    Step 1: Start the program
    Step 2: Initialize the LCD.
    Step 3: Intialise the ADC.Set the port line P0.29 to have the special function ADC input,
    using the PINSEL1 register
    Step 4: call the routine message_display1 to display message”ADC demo” on line 1 of the
    LCD. Call the routine message_display2 to display message”channel2;” on line 2 of
    the LCD.
    Step 5: a) set the control register of the ADC with the following
    specifications;
    Channel number=2;
    ADC clock=3 MHz.
    Select burst mode,
    Conversion clocks required =11clocks
    b) start the ADC conversion
    Step 6: Wait for EOC Once the EOC signal is obtained read the ADC valve.
    Step 7: Display the ADC value on the LCD.
    Repeat from step 4.
    Step8: Stop the program

---
+++ INTERFACING ADC.c
@@ -0,0 +1,38 @@
+ADC
+/****************************************************/
+/* Read ADC channel 2 and display it on LCD Display
+/****************************************************/
+#include "LPC214x.h" /* LPC21xx definitions */
+int ReadADC(char ChannelNumber);
+int main(void)
+{
+int a;
+unsigned char Channel = 2;
+PINSEL1 = 0x04000000; // Select ADC to pin P0.29
+InitializeLCD(); // Initialize LCD
+DisplayLCD(0," ADC DEMO "); // Display message
+DisplayLCD(1,"Channel 2: "); // Display message
+while(1)
+{
+a=ReadADC(Channel); // Read ADC channel 2
+DisplayLCD2Digit(1,10, (a >> 8)); // Display it on 2nd line of LCD
+DisplayLCD2Digit(1,12, (a & 0xff));
+LCDDelay1600();
+}
+}
+//Read ADC data from given channel number
+int ReadADC(char ChannelNumber)
+{
+int val,ch;
+ch = 1<<ChannelNumber;
+AD0CR = 0x00210400 | ch; // Setup A/D: 10-bit AIN @ 3MHz
+AD0CR |= 0x01000000; // Start A/D Conversion
+do
+{
+val = AD0DR2; // Read A/D Data Register
+}
+while ((val & 0x80000000) == 0); // Wait for the conversion to complete
+val = ((val >> 6) & 0x03FF); // Extract the A/D result
+AD0CR &= ~0x01000000; // Stop A/D Conversion
+return(val); // Return the Data Read
+}
